78845ae57b52ce9784d15ee45f29dea30665cb34
dudu-theman <dustanley07@gmail.com>
2026-05-11 21:49:17 -0500

    copy timestamp_utils.py over from moral outrage repo

--- lib/timestamp_utils.py
+++ lib/timestamp_utils.py
@@ -1,4 +1,4 @@
-from datetime import datetime, timezone
+from datetime import UTC, datetime
 
 CREATED_AT_FORMAT: str = "%Y_%m_%d-%H:%M:%S"
 
@@ -6,4 +6,4 @@ CREATED_AT_FORMAT: str = "%Y_%m_%d-%H:%M:%S"
 def get_current_timestamp() -> str:
     """Get the current timestamp in the contract format."""
 
-    return datetime.now(timezone.utc).strftime(CREATED_AT_FORMAT)
+    return datetime.now(UTC).strftime(CREATED_AT_FORMAT)
