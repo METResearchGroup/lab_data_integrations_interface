4de036de323f9452b3f6f08d14b24cc474e254b5
dudu-theman <dustanley07@gmail.com>
2026-05-26 19:49:27 -0500

    enforce bootstrapping

--- terraform/backend/main.tf
+++ terraform/backend/main.tf
@@ -7,9 +7,11 @@ terraform {
   }
 
   backend "s3" {
-    bucket = "lab-data-integrations-tf-state"
-    key = "backend/terraform.tfstate"
-    region = "us-east-2"
+    bucket         = "lab-data-integrations-tf-state"
+    key            = "backend/terraform.tfstate"
+    region         = "us-east-2"
+    dynamodb_table = "tf-state-lock"
+    encrypt        = true
   }
 }
 
@@ -160,41 +162,41 @@ resource "aws_vpc_security_group_ingress_rule" "ecs_from_alb" {
 
 resource "aws_vpc_security_group_egress_rule" "ecs_all" {
   security_group_id = aws_security_group.ecs.id
-  from_port = -1
-  to_port = -1
-  ip_protocol = "-1"
-  cidr_ipv4 = "0.0.0.0/0"
+  from_port         = -1
+  to_port           = -1
+  ip_protocol       = "-1"
+  cidr_ipv4         = "0.0.0.0/0"
 }
 
 resource "aws_lb" "backend" {
-  name = "lab-data-integrations-alb"
-  internal = false
+  name               = "lab-data-integrations-alb"
+  internal           = false
   load_balancer_type = "application"
-  security_groups = [aws_security_group.alb.id]
-  subnets = [aws_subnet.public_a.id, aws_subnet.public_b.id]
+  security_groups    = [aws_security_group.alb.id]
+  subnets            = [aws_subnet.public_a.id, aws_subnet.public_b.id]
 }
 
 resource "aws_lb_target_group" "backend" {
-  name = "lab-data-integrations-tg"
-  port = 8080
-  protocol = "HTTP"
-  vpc_id = aws_vpc.main.id
+  name        = "lab-data-integrations-tg"
+  port        = 8080
+  protocol    = "HTTP"
+  vpc_id      = aws_vpc.main.id
   target_type = "ip"
 
   health_check {
-    path = "/health"
-    healthy_threshold = 2
+    path                = "/health"
+    healthy_threshold   = 2
     unhealthy_threshold = 3
   }
 }
 
 resource "aws_lb_listener" "backend" {
-  load_balancer_arn =  aws_lb.backend.arn
-  port = 80
-  protocol = "HTTP"
+  load_balancer_arn = aws_lb.backend.arn
+  port              = 80
+  protocol          = "HTTP"
 
   default_action {
-    type = "forward"
+    type             = "forward"
     target_group_arn = aws_lb_target_group.backend.arn
   }
 }
@@ -204,21 +206,21 @@ resource "aws_cloudwatch_log_group" "backend" {
 }
 
 resource "aws_ecs_service" "backend" {
-  name = "lab-data-integrations-backend" 
-  cluster = aws_ecs_cluster.backend.id
+  name            = "lab-data-integrations-backend"
+  cluster         = aws_ecs_cluster.backend.id
   task_definition = aws_ecs_task_definition.backend.arn
-  desired_count = 1
-  launch_type = "FARGATE"
+  desired_count   = 1
+  launch_type     = "FARGATE"
 
   network_configuration {
-    subnets = [aws_subnet.public_a.id, aws_subnet.public_b.id]
-    security_groups = [aws_security_group.ecs.id]
+    subnets          = [aws_subnet.public_a.id, aws_subnet.public_b.id]
+    security_groups  = [aws_security_group.ecs.id]
     assign_public_ip = true
   }
 
   load_balancer {
     target_group_arn = aws_lb_target_group.backend.arn
-    container_name = "backend"
-    container_port = 8080
+    container_name   = "backend"
+    container_port   = 8080
   }
 }
