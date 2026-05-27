3759d5896a97cd28367da5af264a58531fe40e25
dudu-theman <dustanley07@gmail.com>
2026-05-26 19:49:41 -0500

    add public access block

--- terraform/backend/bootstrap/main.tf
+++ terraform/backend/bootstrap/main.tf
@@ -22,6 +22,14 @@ resource "aws_s3_bucket_versioning" "tf_state" {
   }
 }
 
+resource "aws_s3_bucket_public_access_block" "tf_state" {
+  bucket                  = aws_s3_bucket.tf_state.id
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
+
 resource "aws_dynamodb_table" "tf_state_lock" {
   name         = "tf-state-lock"
   billing_mode = "PAY_PER_REQUEST"
