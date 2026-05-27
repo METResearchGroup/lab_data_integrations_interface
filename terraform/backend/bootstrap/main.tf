3e9dc0ed653564041ef091b54ac7bbd54d8de30d
dudu-theman <dustanley07@gmail.com>
2026-05-26 19:33:53 -0500

    add s3 bucket versioning

---
+++ terraform/backend/bootstrap/main.tf
@@ -0,0 +1,34 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-east-2"
+}
+
+resource "aws_s3_bucket" "tf_state" {
+  bucket = "lab-data-integrations-tf-state"
+}
+
+resource "aws_s3_bucket_versioning" "tf_state" {
+  bucket = aws_s3_bucket.tf_state.id
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+resource "aws_dynamodb_table" "tf_state_lock" {
+  name         = "tf-state-lock"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "LockID"
+
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+}
