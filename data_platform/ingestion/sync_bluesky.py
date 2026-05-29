0ad47c860fc2c1ab39c5bd0db455f7defa16e7de
mark-torres10 <markptorres1@gmail.com>
2026-05-29 12:15:27 -0500

    update notes

---
+++ data_platform/ingestion/sync_bluesky.py
@@ -0,0 +1,3 @@
+def sync_records():
+    # writes to data_platform/data/bluesky/raw/{timestamp}/{metadata.json, *.parquet}
+    pass
---
+++ data_platform/preprocessing/preprocess_bluesky.py
@@ -0,0 +1,4 @@
+def preprocess_records():
+    # loads from data_platform/data/bluesky/preprocessed/{latest timestamp}/{metadata.json, *.parquet}
+    # writes to data_platform/data/bluesky/preprocessed/{timestamp}/{metadata.json, *.parquet}
+    pass
