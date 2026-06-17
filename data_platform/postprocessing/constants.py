db4cd84edcbad55a4cc40b327c452877570a1e23
dudu-theman <dustanley07@gmail.com>
2026-06-17 14:36:06 -0700

    flesh out functions

---
+++ data_platform/postprocessing/constants.py
@@ -0,0 +1,3 @@
+S3_BUCKET = "lab-data-integrations-interface"
+CURATED_S3_PREFIX = "bluesky/curated"
+URIS_S3_PREFIX = "bluesky/uris"
--- data_platform/postprocessing/postprocess_bluesky.py
+++ data_platform/postprocessing/postprocess_bluesky.py
@@ -1,20 +1,20 @@
 from __future__ import annotations
 
 import shutil
+import tempfile
 from pathlib import Path
 
-from data_platform.aws.s3 import S3
-from data_platform.utils.storage import BlueskyStorageManager, StorageStage
+import pandas as pd
 
-S3_BUCKET = ""  # TODO
-CURATED_S3_PREFIX = "bluesky/curated"
-URIS_S3_PREFIX = "bluesky/uris"
+from data_platform.aws.s3 import S3
+from data_platform.postprocessing.constants import CURATED_S3_PREFIX, S3_BUCKET, URIS_S3_PREFIX
+from data_platform.utils.storage import BlueskyStorageManager, StorageStage
 
 
 def postprocess_bluesky(dataset_id: str, curated_path: Path) -> None:
     s3 = S3()
     _upload_curated(s3, dataset_id, curated_path)
-    _upload_uris(s3, dataset_id)
+    _upload_uris(s3, dataset_id, curated_path)
     _delete_local(dataset_id)
 
 
@@ -23,9 +23,14 @@ def _upload_curated(s3: S3, dataset_id: str, curated_path: Path) -> None:
     s3.upload_file(curated_path, S3_BUCKET, key)
 
 
-def _upload_uris(s3: S3, dataset_id: str) -> None:
-    # TODO: determine URI source and S3 key
-    raise NotImplementedError
+def _upload_uris(s3: S3, dataset_id: str, curated_path: Path) -> None:
+    """Upload URIs from the curated output to S3. URIs are always uploaded as parquet."""
+    uris = pd.read_parquet(curated_path, columns=["uri"])
+    with tempfile.NamedTemporaryFile(suffix=".parquet") as tmp:
+        tmp_path = Path(tmp.name)
+        uris.to_parquet(tmp_path, index=False)
+        key = f"{URIS_S3_PREFIX}/{dataset_id}/uris.parquet"
+        s3.upload_file(tmp_path, S3_BUCKET, key)
 
 
 def _delete_local(dataset_id: str) -> None:
